7a26966177de6cea958b552866a8f1ce55dd0261
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-14 09:52:27 -0500

    check connection between classes

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const user = new User("example@mail.com")
 
 // console.log(user.email)
 user.getEmail()
+
+const maliKrug = user.addRoute("mali krug");
+
+console.dir(maliKrug)
+maliKrug.getName()
+maliKrug.addPin(123, 123)
+console.dir(maliKrug)
--- modules/user.js
+++ modules/user.js
@@ -1,10 +1,19 @@
+import Route from "./route.js";
+
 export default class User{
     _email;
+    _routes = []
     constructor(email){
         this._email = email
     }
 
     getEmail(){
         console.log(this._email)
+    }
+
+    addRoute(routeName){
+        const route = new Route(routeName)
+        this._routes.push(route)
+        return route
     }
 }

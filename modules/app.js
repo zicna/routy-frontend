9d3058621bf6afcbd7a99102c4a5455986c95f44
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-15 15:36:03 -0500

    handle setting and getting token in localStorage

--- modules/app.js
+++ modules/app.js
@@ -1,5 +1,14 @@
 class App{
 
+    setToken(token){
+        localStorage.setItem("token", token)
+    }
+
+    getToken(){
+        return localStorage.getItem("token")
+    }
+
+
 }
 
 export default App

5428102568c8751f8cdcc3fabc7fc1bac58e115e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-19 10:29:00 -0500

    add proper error handeling if errorCallback is called for .getCurrentposition()

--- src/view/mapView.js
+++ src/view/mapView.js
@@ -1,27 +1,39 @@
-const mapContainer = document.querySelector(".map-container")
+const mapContainer = document.querySelector('.map-container')
 
-class MapView{
-    _lat;
-    _long;
-render(){
-    const success = (geolocationPosition) => {
-        console.log("success")
-        console.dir(geolocationPosition)
-        this._lat = geolocationPosition.coords.latitude
-        this._long = geolocationPosition.coords.longitude
-        mapContainer.innerHTML = `
-        <a target="_blank" href="https://www.google.com/maps/@${this._lat},${this._long}z">
-            click here to go to your location 
-        </a>
-        `
-        debugger
+class MapView {
+  _lat
+  _long
+  render() {
+    const successCallback = (position) => {
+      this._lat = position.coords.latitude
+      this._long = position.coords.longitude
+      this.loadMap()
     }
+// !error handling if erorCallback is called
+    const errorCallback = (error) => {
+      if (error.code == 1) {
+        mapContainer.innerHTML =
+          "You've decided not to share your position, but it's OK. We won't ask you again."
+      } else if (error.code == 2) {
+        mapContainer.innerHTML =
+          "The network is down or the positioning service can't be reached."
+      } else if (error.code == 3) {
+        mapContainer.innerHTML =
+          'The attempt timed out before it could get the location data.'
+      } else {
+        mapContainer.innerHTML = 'Geolocation failed due to unknown error.'
+      }
+    }
+
+    if (!navigator.geolocation) {
+      return alert('Sorry your browser does not support geolocation')
+    }
+    navigator.geolocation.getCurrentpositiontion(successCallback, errorCallback)
+  }
+
+  loadMap(){
     
-    const error = () => {
-        console.log("error")
-    }
-    navigator.geolocation.getCurrentPosition(success, error)
-}
+  }
 }
 
 export default new MapView()

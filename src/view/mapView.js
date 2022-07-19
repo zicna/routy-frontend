2bc4e8c4da756c1d08a533d59631ce9ab519f5aa
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-19 15:51:13 -0500

    refactor mapView.js

--- src/view/mapView.js
+++ src/view/mapView.js
@@ -1,41 +1,46 @@
 const mapContainer = document.querySelector('.map-container')
 
 class MapView {
-  _lat
-  _long
+  long
+  lat
   render() {
-    const successCallback = (position) => {
-      this._lat = position.coords.latitude
-      this._long = position.coords.longitude
-      this.loadMap()
-    }
-// !error handling if erorCallback is called
-    const errorCallback = (error) => {
-      if (error.code == 1) {
-        mapContainer.innerHTML =
-          "You've decided not to share your position, but it's OK. We won't ask you again."
-      } else if (error.code == 2) {
-        mapContainer.innerHTML =
-          "The network is down or the positioning service can't be reached."
-      } else if (error.code == 3) {
-        mapContainer.innerHTML =
-          'The attempt timed out before it could get the location data.'
-      } else {
-        mapContainer.innerHTML = 'Geolocation failed due to unknown error.'
-      }
-    }
-
-    if (!navigator.geolocation) {
-      return alert('Sorry your browser does not support geolocation')
-    }
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
+    this.#getLocation()
+    
   }
 
-  loadMap(){
-    const coors = [this._lat, this._long]
+  #getLocation() {
+    if (!navigator.geolocation)
+      return alert('Sorry your browser does not support geolocation')
+    navigator.geolocation.getCurrentPosition(
+      this.#successCallback.bind(this),
+      this.#errorCallback.bind(this)
+    )
+  }
+  #successCallback(position) {
+    this.lat = position.coords.latitude
+    this.long = position.coords.longitude
+    
+    this.#loadMap()
+  }
+  #errorCallback(error) {
+    if (error.code == 1) {
+      mapContainer.innerHTML =
+        "You've decided not to share your position, but it's OK. We won't ask you again."
+    } else if (error.code == 2) {
+      mapContainer.innerHTML =
+        "The network is down or the positioning service can't be reached."
+    } else if (error.code == 3) {
+      mapContainer.innerHTML =
+        'The attempt timed out before it could get the location data.'
+    } else {
+      mapContainer.innerHTML = 'Geolocation failed due to unknown error.'
+    }
+  }
+
+  #loadMap() {
+    const coors = [this.lat, this.long]
     const zoomLevel = 10
     const map = L.map(mapContainer).setView(coors, zoomLevel)
-    // console.dir(map)
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:

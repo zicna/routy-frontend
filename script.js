5c2062cbcd24254b56921daddbde3597d9110e19
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-21 09:29:44 -0500

    all UI clicks handle through toggle functions with 'hide' class

--- script.js
+++ script.js
@@ -15,31 +15,37 @@ const btnSignin = document.getElementById('btn_signin')
 const btnAddRoute = document.getElementById('add_route')
 
 // const routesContainer = document.querySelector('.routes-container')
-
 //* user form
-const form = document.querySelector('.form')
+const form = document.getElementById('user_form')
 const formEmail = document.querySelector('#email')
 const formPassword = document.querySelector('#password')
 const formPasswordConfirmation = document.querySelector(
   '#password_confirmation'
 )
 const submitUser = document.getElementById('submit_user')
-
+const cancelSubmitUser = document.getElementById("cancel_submit_user")
 //* route form and buttons
 const routeForm = document.getElementById('route_form')
 const routeName = document.getElementById('route_name')
-const routeList = document.querySelector(".route-list")
-
+const routeList = document.querySelector('.route-list')
 
 // ***************************************************
-const handleAddRouteClick = () => {
-  btnAddRoute.style.display = 'none'
-  routeForm.style.display = 'block'
+// * toggle hide class to handle clicks on page
+const toggleHideOne = () => {
+  btnAddRoute.classList.toggle('hide')
+  routeForm.classList.toggle('hide')
 }
-const handleSigninClick = () => {
-  btnSignin.style.display = 'none'
-  form.style.display = 'block'
+const toggleHideTwo = () => {
+  form.reset()
+  btnSignin.classList.toggle('hide')
+  form.classList.toggle('hide')
 }
+
+const toggleHideThree = () => {
+  btnAddRoute.classList.toggle('hide')
+  form.classList.toggle('hide')
+}
+// ***************************************************
 
 const handleUserSubmit = async function (event) {
   try {
@@ -54,64 +60,65 @@ const handleUserSubmit = async function (event) {
     }
 
     await model.loadUser(userObject)
-    if(!model.state.token) throw new Error("something went wrong")
+    if (!model.state.token) throw new Error('something went wrong')
 
     userView.render(model.state.user)
 
     event.target.reset()
     // !call something to load map with current user navigation
     mapView.render()
-    btnAddRoute.style.display = 'block'
-    form.style.display = 'none'
-    
+
+    toggleHideThree()
+
   } catch (error) {
     alert(error)
     event.target.reset()
   }
 }
 
-const handleRouteSubmit = async function(event){
+const handleRouteSubmit = async function (event) {
   event.preventDefault()
 
   const dataObject = {
     user: { route_name: routeName.value },
   }
-try {
-  await model.createRoute(dataObject)
-  routeView.render(model.state.userRoutes)
-} catch (error) {
-  alert(error)
-}
+  try {
+    await model.createRoute(dataObject)
+    routeView.render(model.state.userRoutes)
+  } catch (error) {
+    alert(error)
+  } finally {
+    toggleHideOne()
+  }
   event.target.reset()
 }
 
 // * using event prapagation to handle clicking on delete btn
-// ! not async callback because we will have positive approach=> 
+// ! not async callback because we will have positive approach=>
 // !remove form UI and trust it will be removed from backend as well
-const handleDeleteRouteClick = function(event){
+const handleDeleteRouteClick = function (event) {
   event.preventDefault()
-// ! quard clause if click event happens on ul outside of button
-  if(event.target.classList.contains("btn-delete")){
+  // ! quard clause if click event happens on ul outside of button
+  if (event.target.classList.contains('btn-delete')) {
     const route = {
-      user: {route_id: event.target.parentElement.dataset.routeId}
+      user: { route_id: event.target.parentElement.dataset.routeId },
     }
-  
+
     try {
       event.target.parentElement.remove()
       model.deleteRoute(route)
     } catch (error) {
       alert(error)
     }
-  }else if(event.target.classList.contains("btn-add-pin")){
-
+  } else if (event.target.classList.contains('btn-add-pin')) {
   }
-  
 }
 // * Event Listeners
 // *****************************************************************
 form.addEventListener('submit', handleUserSubmit)
 routeForm.addEventListener('submit', handleRouteSubmit)
-btnSignin.addEventListener('click', handleSigninClick)
-btnAddRoute.addEventListener('click', handleAddRouteClick)
-routeList.addEventListener("click", handleDeleteRouteClick)
+btnSignin.addEventListener('click', toggleHideTwo)
+cancelSubmitUser.addEventListener("click", toggleHideTwo)
+btnAddRoute.addEventListener('click', toggleHideOne)
+routeList.addEventListener('click', handleDeleteRouteClick)
 // *****************************************************************

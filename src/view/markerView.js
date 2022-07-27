ca232efee5e3cee2cb624889431a643844c340bc
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-27 14:37:57 -0500

    add markerView method for adding newly created marker

--- src/view/markerView.js
+++ src/view/markerView.js
@@ -1,4 +1,4 @@
-const markerList = document.getElementById('djsdksj')
+const markerList = document.querySelector(".marker-list")
 
 class MarkerView {
   #data
@@ -20,6 +20,19 @@ class MarkerView {
             `
       markerList.appendChild(li)
     })
+  }
+
+  addNewMarker(marker){
+    const li = document.createElement('li')
+      li.dataset.markerId = marker.id
+      li.innerHTML = `
+                <p>${marker.name}</p>
+                Category: <span>${marker.category}</span>
+                <p>Description: ${marker.description}</p> 
+                <button class="btn btn-delete">X</button>
+                <button class="btn btn-load-marker">L</button>
+            `
+      markerList.appendChild(li)
   }
 
   clear() {

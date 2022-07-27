f906ed1752ef4cab8d8796a02b37ba7f37feee61
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-27 15:49:56 -0500

    add removeMarker method

--- src/view/markerView.js
+++ src/view/markerView.js
@@ -1,4 +1,4 @@
-const markerList = document.querySelector(".marker-list")
+const markerList = document.querySelector('.marker-list')
 
 class MarkerView {
   #data
@@ -15,24 +15,30 @@ class MarkerView {
                 <p>${marker.name}</p>
                 Category: <span>${marker.category}</span>
                 <p>Description: ${marker.description}</p> 
-                <button class="btn btn-delete">X</button>
+                <button class="btn btn-delete-marker">X</button>
                 <button class="btn btn-load-marker">L</button>
             `
       markerList.appendChild(li)
     })
   }
 
-  addNewMarker(marker){
+  addNewMarker(marker) {
     const li = document.createElement('li')
-      li.dataset.markerId = marker.id
-      li.innerHTML = `
+    li.dataset.markerId = marker.id
+    li.innerHTML = `
                 <p>${marker.name}</p>
                 Category: <span>${marker.category}</span>
                 <p>Description: ${marker.description}</p> 
                 <button class="btn btn-delete">X</button>
                 <button class="btn btn-load-marker">L</button>
             `
-      markerList.appendChild(li)
+    markerList.appendChild(li)
+  }
+
+  removeMarker(id) {
+    markerList.childNodes.forEach((li) => {
+      if (li.dataset.markerId == id) li.remove()
+    })
   }
 
   clear() {

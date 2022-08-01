9ce2fce32814b69f199b44c73c0a7d8559f5797a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-08-01 12:53:18 -0500

    style user credentials

--- src/view/userView.js
+++ src/view/userView.js
@@ -1,29 +1,30 @@
 // ! mistake view should nOT know anything about state (model)
 // import { state } from './model.js'
 
-const credentials = document.querySelector(".credentials")
-const messageContainer = document.querySelector(".message-container")
-import {userName} from '../helpers/viewHelper.js'
+const credentials = document.querySelector('.credentials')
+const messageContainer = document.querySelector('.message-container')
+import { userName } from '../helpers/viewHelper.js'
 
-class UserView{
-    _data;
+class UserView {
+  _data
 
-    render(data){
-        this._data = data
-        this.generateMarkup()
-    }
-    generateMarkup(){
-        credentials.innerHTML = `
-        <p>
-        <ion-icon name="person-outline"></ion-icon>
-            ${userName(this._data.user.email)}
-        </p>
+  render(data) {
+    this._data = data
+    this.generateMarkup()
+  }
+  generateMarkup() {
+    credentials.innerHTML = `
+            <div class="user">
+                <ion-icon name="person-outline"></ion-icon>
+                    ${userName(this._data.user.email)}
+            </div>
+            <button class="btn-small btn-logout" id="logout_user">Log Out</button>
         `
-    }
-    
-    logOutUser(){
-        credentials.innerHTML= ``
-    }
+  }
+
+  logOutUser() {
+    credentials.innerHTML = ``
+  }
 }
 
-export default new UserView();
+export default new UserView()

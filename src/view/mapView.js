3ed585be70196b5b12639b2ae45c6151f0a7d111
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-08-04 11:26:50 -0500

    change where do we display geolocation errors

--- src/view/mapView.js
+++ src/view/mapView.js
@@ -86,16 +86,16 @@ class MapView {
   }
   #errorCallback(error) {
     if (error.code == 1) {
-      mapContainer.innerHTML =
+      markerMsgContainer.innerHTML =
         "You've decided not to share your position, but it's OK. We won't ask you again."
     } else if (error.code == 2) {
-      mapContainer.innerHTML =
+      markerMsgContainer.innerHTML =
         "The network is down or the positioning service can't be reached."
     } else if (error.code == 3) {
-      mapContainer.innerHTML =
+      markerMsgContainer.innerHTML =
         'The attempt timed out before it could get the location data.'
     } else {
-      mapContainer.innerHTML = 'Geolocation failed due to unknown error.'
+      markerMsgContainer.innerHTML = 'Geolocation failed due to unknown error.'
     }
   }
 

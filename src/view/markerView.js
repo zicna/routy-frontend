40cfcff3c2a5dc85da48c08068fbea552a3853c3
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-08-04 11:28:41 -0500

    move API of class up and marke it with comments

--- src/view/markerView.js
+++ src/view/markerView.js
@@ -3,39 +3,12 @@ const markerMsgContainer = document.getElementById('marker-msg-container')
 
 class MarkerView {
   #data
+
+  // ***************** API of a MarkerView class ********************
+
   render(data) {
     this.#data = data
     this.#generateMarkap()
-  }
-
-  
-
-  #generateMarkap() {
-    if (this.#data.length == 0) {
-      markerMsgContainer.innerHTML = `
-      <p>
-      Please wait for the map to be loaded. Click on the map to display the new marker form.
-      </p>
-      `
-      return
-    }
-    this.#data.forEach((marker) => {
-      const li = document.createElement('li')
-      li.dataset.markerId = marker.id
-      li.innerHTML = `
-        <div class="card marker-${marker.category}">
-          <div class="card-main">
-            <div class="name-display">${marker.name}</div>
-            <div class="text-display">${marker.description}</div> 
-          </div>
-          <div card="card-btns">
-            <button class="btn-marker btn-small btn-cancel">del</button>
-            <button class="btn-marker btn-small btn-load-marker">load</button>
-          </div>
-        </div>
-      `
-      markerList.appendChild(li)
-    })
   }
 
   addNewMarker(marker) {
@@ -64,6 +37,36 @@ class MarkerView {
 
   clear() {
     markerList.innerHTML = ''
+  }
+
+  // *************************************
+
+  #generateMarkap() {
+    if (this.#data.length == 0) {
+      markerMsgContainer.innerHTML = `
+      <p>
+      Please wait for the map to be loaded. Click on the map to display the new marker form.
+      </p>
+      `
+      return
+    }
+    this.#data.forEach((marker) => {
+      const li = document.createElement('li')
+      li.dataset.markerId = marker.id
+      li.innerHTML = `
+        <div class="card marker-${marker.category}">
+          <div class="card-main">
+            <div class="name-display">${marker.name}</div>
+            <div class="text-display">${marker.description}</div> 
+          </div>
+          <div card="card-btns">
+            <button class="btn-marker btn-small btn-cancel">del</button>
+            <button class="btn-marker btn-small btn-load-marker">load</button>
+          </div>
+        </div>
+      `
+      markerList.appendChild(li)
+    })
   }
 }
 
